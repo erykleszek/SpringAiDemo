eb03645517ee17acb89013894e01317b99697c8e
Lesio <e.leszek.one@gmail.com>
2025-04-01 23:18:36 +0200

    add ImageService,modify GenAIController, pom.xml

--- src/main/java/com/ai/springaidemo/GenAIController.java
+++ src/main/java/com/ai/springaidemo/GenAIController.java
@@ -1,19 +1,31 @@
 package com.ai.springaidemo;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+import java.io.IOException;
+
+@RestController
 public class GenAIController {
 
-    ChatService chatService;
-    public GenAIController(ChatService chatService) {
+    private final ChatService chatService;
+    private final ImageService imageService;
+    public GenAIController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
     public String getResponse(@RequestParam String prompt) {
         return chatService.getResponse(prompt);
+    }
+    @GetMapping("generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
     }
 }
